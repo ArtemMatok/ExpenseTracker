993677433ebe37d31a988a0908db538ea2144491
ArtemMatok <125476461+ArtemMatok@users.noreply.github.com>
2023-07-24 23:07:03 +0300

    Add files via upload

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,106 @@
+﻿using Expense_Tracker.Data;
+using Expense_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Syncfusion.EJ2.Charts;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace Expense_Tracker.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            //Last 7 day
+            DateTime StartDate = DateTime.Today.AddDays(-6);
+            DateTime EndDate = DateTime.Today;
+            List<Transaction> SelectedTransactions = await _context.Transactions
+                .Include(x => x.Category).Where(x=>x.Date>=StartDate && x.Date<=EndDate).ToListAsync();
+
+            //Загальний прибуток 
+            int TotalIncome = SelectedTransactions.Where(x => x.Category.Type == "Income").Sum(x => x.Amount);
+            ViewBag.TotalIncome = TotalIncome.ToString("C", new CultureInfo("uk-UA"));
+
+            //Загальні Витрати
+            int TotalExpense = SelectedTransactions.Where(x => x.Category.Type == "Expense").Sum(x => x.Amount);
+            ViewBag.TotalExpense = TotalExpense.ToString("C", new CultureInfo("uk-UA"));
+
+            //Баланс
+            int Balance = TotalIncome - TotalExpense;
+            ViewBag.Balance = Balance.ToString("C", new CultureInfo("uk-UA"));
+
+            CultureInfo culture = CultureInfo.CreateSpecificCulture("uk-UA");
+            culture.NumberFormat.CurrencyNegativePattern = 1;
+            ViewBag.Balance = String.Format(culture, "{0:C}", Balance);
+
+            //Діаграма
+            ViewBag.DoughnutCahrtData = SelectedTransactions
+                .Where(x => x.Category.Type == "Expense").GroupBy(x => x.CategoryId)
+                .Select(x => new 
+                {
+                    categoryTitleWithIcon = x.First().Category.Icon +" "+x.First().Category.Title,
+                    amount = x.Sum(x=>x.Amount),
+                    formattedAdmount = x.Sum(x=>x.Amount).ToString("C", new CultureInfo("uk-UA")),
+
+                }).OrderByDescending(x=>x.amount).ToList();
+
+            // Заробіток vs Трати
+            //Заробіток
+            List<SplineChartData> IncomeSummary = SelectedTransactions
+                .Where(x => x.Category.Type == "Income")
+                .GroupBy(x=>x.Date)
+                .Select(k => new SplineChartData()
+                {
+                    day = k.First().Date.ToString("dd-MMM"),
+                    income = k.Sum(x=>x.Amount)
+                }).ToList();
+            //Трати
+            List<SplineChartData> ExpenseSummary = SelectedTransactions
+                .Where(x => x.Category.Type == "Expense")
+                .GroupBy(x => x.Date)
+                .Select(k => new SplineChartData()
+                {
+                    day = k.First().Date.ToString("dd-MMM"),
+                    expense = k.Sum(x => x.Amount)
+                }).ToList();
+
+            //комбінування трат і прибутків
+            string[] Last7Days = Enumerable.Range(0, 7)
+                   .Select(i => StartDate.AddDays(i).ToString("dd-MMM"))
+                   .ToArray();
+
+            ViewBag.SplineChartData = from day in Last7Days
+                                      join income in IncomeSummary on day equals income.day into dayIncomeJoined
+                                      from income in dayIncomeJoined.DefaultIfEmpty()
+                                      join expense in ExpenseSummary on day equals expense.day into expenseJoined
+                                      from expense in expenseJoined.DefaultIfEmpty()
+                                      select new
+                                      {
+                                          day = day,
+                                          income = income == null ? 0 : income.income,
+                                          expense = expense == null ? 0 : expense.expense,
+                                      };
+            //Останні транзакції
+            ViewBag.RecentTransactions = await _context.Transactions
+                .Include(x => x.Category).OrderByDescending(x => x.Date)
+                .Take(5).ToListAsync();
+
+            return View();
+        }
+    }
+    public class SplineChartData
+    {
+        public string day;
+        public int income;
+        public int expense;
+    }
+
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        #region Category
+            public int CategoryId { get; set; }
+            public Category? Category { get; set; }
+        #endregion
+
+
+        public int Amount { get; set; }
+        [Column(TypeName ="nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        [NotMapped]
+        public string? CategoryTitleWithIcon 
+        { 
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+
+
+        [NotMapped]
+        public string? FormatAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Type=="Expense") ? "- " : "+ ")+Amount.ToString("C", new CultureInfo("uk-UA"));
+            }
+        }
+    }
+}
